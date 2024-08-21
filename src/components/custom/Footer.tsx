a7dd34b9d2fbfe0ec9e221bd32fc8540e42c1199
juanigarciadev <juanigarciadev@gmail.com>
2024-08-21 11:49:25 -0300

    Minor color fixes in hero background animation

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -28,7 +28,7 @@ const Footer = () => {
         },
     ]
     return (
-        <footer className="relative w-screen h-auto bg-main rounded-lg py-8 px-96 xl:px-16 lg:px-8 sm:px-2">
+        <footer className="relative w-screen h-auto bg-main py-8 px-96 xl:px-16 lg:px-8 sm:px-2">
             <div className="w-full h-auto bg-light border border-cardBorder rounded-lg">
                 <div className="flex items-center justify-between w-full p-4 md:flex-col md:gap-4">
                     <span className="text-xs tracking-widest">Everything, made with love. Always.</span>
