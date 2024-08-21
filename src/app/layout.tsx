1bdca7f25b8fc554407428a6f02cfc747ff4107a
juanigarciadev <juanigarciadev@gmail.com>
2024-08-21 11:55:26 -0300

    Minor fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className="dark">
             <body className="font-serif font-light bg-main text-white overflow-x-hidden">
                 <Header />
                 {children}
